5f019267ed72fbfa8499fbaca0c3d87d27b89586
icedoom888 <alby.penny@gmail.com>
2022-01-27 14:08:26 +0100

    First try

--- quick_demo.py
+++ quick_demo.py
@@ -28,7 +28,8 @@ data = load_data()
 countries = data.index
 
 sel_countries = st.multiselect('Select some Countries', countries)
-c_data = data.loc[sel_countries].transpose
+# sel_countries = ['Albania', 'Colombia']
+c_data = data.loc[sel_countries]
 
 with st.container():
     st.write(f"Showing {sel_countries} population data")
