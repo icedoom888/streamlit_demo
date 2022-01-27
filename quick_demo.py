aeedd0f7b92427967f8ef3fdf1b41a59c36fb31a
icedoom888 <alby.penny@gmail.com>
2022-01-27 11:39:26 +0100

    First try

---
+++ quick_demo.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('World Population')
+
+# Load data
+@st.cache
+def load_data():
+    df = pd.read_csv('population_total.csv')
+    df = df.set_index('country')
+    return df
+
+data = load_data()
+countries = data.index
+
+st.selectbox('Select a Country',
+             countries)
+
