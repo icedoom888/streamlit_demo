40a861004f399d5ce9914577dc69f113b9a00028
icedoom888 <alby.penny@gmail.com>
2022-01-27 14:00:35 +0100

    First try

--- quick_demo.py
+++ quick_demo.py
@@ -4,22 +4,35 @@ import numpy as np
 
 st.title('World Population')
 
+
+def convert_str_to_number(x):
+    total_stars = 0
+    num_map = {'K': 1000, 'M': 1000000, 'B': 1000000000}
+    if x.isdigit():
+        total_stars = int(x)
+    else:
+        if len(x) > 1:
+            total_stars = float(x[:-1]) * num_map.get(x[-1].upper(), 1)
+    return int(total_stars)
+
 # Load data
 @st.cache
 def load_data():
     df = pd.read_csv('population_total.csv')
     df = df.set_index('country')
+    for country in df.index:
+        df.loc[country] = df.loc[country].map(lambda x: convert_str_to_number(x))
     return df
 
 data = load_data()
 countries = data.index
 
 country = st.selectbox('Select a Country', countries)
-
 c_data = data.loc[country]
+
 with st.container():
     st.write(f"Showing {country} population data")
     # You can call any Streamlit command, including custom components:
-    st.area_chart(data=c_data)
+    st.area_chart(data=c_data, height=10, width=20)
 
 
