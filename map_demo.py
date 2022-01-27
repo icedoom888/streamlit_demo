7b0d2f992238e87ebe722cee76457c441d2e585c
icedoom888 <alby.penny@gmail.com>
2022-01-27 17:37:01 +0100

    Test altair map

--- map_demo.py
+++ map_demo.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import altair as alt
 import pandas as pd
+from vega_datasets import vd
+
 
 st.title('World Population')
-
 
 def convert_str_to_number(x):
     total_stars = 0
@@ -23,25 +24,30 @@ def load_data():
     df.index.name = None
     for country in df.index:
         df.loc[country] = df.loc[country].map(lambda x: convert_str_to_number(x))
-    return df
+
+    df = df.T
+    df.index = df.index.map(int)
+
+    return df.T
 
 data = load_data()
 countries = data.index
 year = st.slider('Pick a year', min_value=min((data.T.index.tolist())), max_value=max((data.T.index.tolist())))
-
 y_data = data.loc[:, year]
 
-topo = alt.topo_feature(data.us_10m.url, 'countries')
+topo = alt.topo_feature(vd.world_110m.url, 'countries')
 source = y_data
 
-alt.Chart(topo).mark_geoshape().encode(
-    color='rate:Q'
-).transform_lookup(
-    lookup='id',
-    from_=alt.LookupData(source, 'id', ['rate'])
-).project(
-    type='equirectangular'
-).properties(
-    width=500,
-    height=300
-)
+chart = alt.Chart(topo).mark_geoshape().encode(
+    color='rate:Q')
+# ).transform_lookup(
+#     lookup='id',
+#     from_=alt.LookupData(source, 'id', ['rate'])
+# ).project(
+#     type='equirectangular'
+# ).properties(
+#     width=500,
+#     height=300
+# )
+
+st.write(chart)
